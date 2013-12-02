0e46ecd7c3ffb634fa27993f859e90afdd454100
sunzhaoping <sunzhaoping@gmail.com>
2013-12-02 12:33:02 +0900

    sound bug fixed

--- CocosDenshion/ios/CDAudioManager.m
+++ CocosDenshion/ios/CDAudioManager.m
@@ -569,6 +569,9 @@ static BOOL configured = FALSE;
 
 -(void) pauseBackgroundMusic
 {
+    if (![self.backgroundMusic isPlaying]) {
+         return;
+    }
     [self.backgroundMusic pause];
 }    
 
